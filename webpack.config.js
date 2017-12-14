0009ba5fd4e7cf4a80d48821d5b517a5dfe6bdb6
medson10 <medsonoliveira@outlook.com.br>
2017-12-13 23:18:37 -0200

    Update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,30 @@
+var path = require('path');
+
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+  template: './public/index.html',
+  filename: 'index.html',
+  inject: 'body'
+})
+
 module.exports = {
   entry: './app/App.js',
   output: {
-    filename: 'public/bundle.js'
+    path: path.resolve('public'),
+    filename: 'bundle.js'
   },
   module: {
     loaders: [
       {
         test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['react']
+        }
+      },
+      {
+        test: /\.jsx$/,
         exclude: /node_modules/,
         loader: 'babel-loader',
         query: {
