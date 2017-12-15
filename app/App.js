d4e55452788e2014e22834895a324216d8ea8aeb
medson10 <medsonoliveira@outlook.com.br>
2017-12-14 23:29:57 -0200

    Remove useless style

--- app/App.js
+++ app/App.js
@@ -3,12 +3,4 @@ import ReactDOM from 'react-dom';
 
 import Github from './components/Github.jsx';
 
-const containerStyle = {
-  paddingLeft: '150px',
-  backgroundColor: '#e8e8e8',
-  paddingRight: '150px',
-  height: '80%',
-  width: '80%'
-}
-
 ReactDOM.render(<Github />, document.getElementById('app'));
